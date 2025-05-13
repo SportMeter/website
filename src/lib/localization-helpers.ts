fd07e5bb5b6e4f78cf07ba57870bbfdc178b07fd
Mauro <mauro@eijsenring.com>
2025-05-13 22:02:49 +0200

    Introduce dutch language

--- src/lib/localization-helpers.ts
+++ src/lib/localization-helpers.ts
@@ -2,19 +2,19 @@ import contacts from "@/content/global/contacts.json";
 import footerEn from "@/content/global/en/footer.json";
 import headerEn from "@/content/global/en/header.json";
 import seoEn from "@/content/global/en/seo.json";
-import footerIt from "@/content/global/it/footer.json";
-import headerIt from "@/content/global/it/header.json";
-import seoIt from "@/content/global/it/seo.json";
+import footerNl from "@/content/global/nl/footer.json";
+import headerNl from "@/content/global/nl/header.json";
+import seoNl from "@/content/global/nl/seo.json";
 import style from "@/content/global/style.json";
 import widget from "@/content/global/widget.json";
 import { defaultLocale, locales } from "site.config";
 
 const settings: Record<string, LocalizedSettings> = {
-	it: {
-		header: headerIt,
-		footer: footerIt,
+	nl: {
+		header: headerNl,
+		footer: footerNl,
 		contacts: contacts,
-		seo: seoIt,
+		seo: seoNl,
 		style: style,
 		widget: widget,
 	},
