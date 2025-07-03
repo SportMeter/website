e3afdd0061d7d0889a798db1f474ec2008ddbd73
Mauro Eijsenring <mauro@eijsenring.com>
2025-07-03 15:44:42 +0200

    update og image

--- src/pages/[...slug]/og.png.ts
+++ src/pages/[...slug]/og.png.ts
@@ -1,20 +1,22 @@
 import { getCollectionStaticPaths } from "@/lib/collection-helpers";
-import generateOgImage from "@/lib/og-image-generator";
+import ogImage from "../../assets/global/og-image.jpeg";
 import type { APIRoute } from "astro";
-import { siteTitle } from "site";
+import sharp from "sharp";
 
 export const prerender = true;
 
-export const GET: APIRoute<CollectionProps<"pages">> = async ({ props: { data: page } }) => {
-	const response = await generateOgImage(page?.data.title, siteTitle, page?.data.lastUpdateDate);
-	return new Response(response, {
-		status: 200,
-		headers: {
-			"Content-Type": "image/png",
-		},
-	});
+export const GET: APIRoute<CollectionProps<"pages">> = async ({}) => {
+  const buffer = await sharp(ogImage.src).toBuffer();
+
+  const response = buffer;
+  return new Response(response, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
 };
 
 export async function getStaticPaths() {
-	return await getCollectionStaticPaths("pages");
+  return await getCollectionStaticPaths("pages");
 }
